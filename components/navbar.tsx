92996dccb231bbdd91d49a65590fff293a5c2a91
MichaelBosciglio <55165965+MichaelBosciglio@users.noreply.github.com>
2024-04-01 18:28:20 -0400

    Navbar Icons/Links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,39 +20,40 @@ import clsx from "clsx";
 
 import { ThemeSwitch } from "@/components/theme-switch";
 import {
-	TwitterIcon,
 	GithubIcon,
-	DiscordIcon,
+	LinkedinIcon,
 	HeartFilledIcon,
 	SearchIcon,
 } from "@/components/icons";
 
 import { Logo } from "@/components/icons";
+import {Avatar, AvatarGroup, AvatarIcon} from "@nextui-org/react";
+
 
 export const Navbar = () => {
-	const searchInput = (
-		<Input
-			aria-label="Search"
-			classNames={{
-				inputWrapper: "bg-default-100",
-				input: "text-sm",
-			}}
-			endContent={
-				<Kbd className="hidden lg:inline-block" keys={["command"]}>
-					K
-				</Kbd>
-			}
-			labelPlacement="outside"
-			placeholder="Search..."
-			startContent={
-				<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-			}
-			type="search"
-		/>
-	);
+	// const searchInput = (
+	// 	<Input
+	// 		aria-label="Search"
+	// 		classNames={{
+	// 			inputWrapper: "bg-default-100",
+	// 			input: "text-sm",
+	// 		}}
+	// 		endContent={
+	// 			<Kbd className="hidden lg:inline-block" keys={["command"]}>
+	// 				K
+	// 			</Kbd>
+	// 		}
+	// 		labelPlacement="outside"
+	// 		placeholder="Search..."
+	// 		startContent={
+	// 			<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+	// 		}
+	// 		type="search"
+	// 	/>
+	// );
 
 	return (
-		<NextUINavbar maxWidth="xl" position="sticky">
+		<NextUINavbar isBordered maxWidth="xl" position="sticky">
 			<NavbarContent className="basis-1/5 sm:basis-full" justify="start">
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
@@ -60,7 +61,7 @@ export const Navbar = () => {
 						<p className="font-bold text-inherit">ACME</p>
 					</NextLink>
 				</NavbarBrand>
-				<ul className="hidden lg:flex gap-4 justify-start ml-2">
+				<ul className="flex gap-4 justify-start ml-2">
 					{siteConfig.navItems.map((item) => (
 						<NavbarItem key={item.href}>
 							<NextLink
@@ -79,46 +80,20 @@ export const Navbar = () => {
 			</NavbarContent>
 
 			<NavbarContent
-				className="hidden sm:flex basis-1/5 sm:basis-full"
 				justify="end"
 			>
-				<NavbarItem className="hidden sm:flex gap-2">
-					<Link isExternal href={siteConfig.links.twitter} aria-label="Twitter">
-						<TwitterIcon className="text-default-500" />
-					</Link>
-					<Link isExternal href={siteConfig.links.discord} aria-label="Discord">
-						<DiscordIcon className="text-default-500" />
+				<NavbarItem className="flex gap-2">
+					<Link isExternal href={siteConfig.links.linkedin} aria-label="Linkedin">
+						<LinkedinIcon className="text-default-500" />
 					</Link>
 					<Link isExternal href={siteConfig.links.github} aria-label="Github">
 						<GithubIcon className="text-default-500" />
 					</Link>
 					<ThemeSwitch />
 				</NavbarItem>
-				<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-				<NavbarItem className="hidden md:flex">
-					<Button
-            isExternal
-						as={Link}
-						className="text-sm font-normal text-default-600 bg-default-100"
-						href={siteConfig.links.sponsor}
-						startContent={<HeartFilledIcon className="text-danger" />}
-						variant="flat"
-					>
-						Sponsor
-					</Button>
-				</NavbarItem>
 			</NavbarContent>
-
-			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-				<Link isExternal href={siteConfig.links.github} aria-label="Github">
-					<GithubIcon className="text-default-500" />
-				</Link>
-				<ThemeSwitch />
-				<NavbarMenuToggle />
-			</NavbarContent>
-
 			<NavbarMenu>
-				{searchInput}
+				{/* {searchInput} */}
 				<div className="mx-4 mt-2 flex flex-col gap-2">
 					{siteConfig.navMenuItems.map((item, index) => (
 						<NavbarMenuItem key={`${item}-${index}`}>
@@ -138,7 +113,18 @@ export const Navbar = () => {
 						</NavbarMenuItem>
 					))}
 				</div>
+			
 			</NavbarMenu>
+			<Avatar
+				isBordered
+				disabled
+				as="button"
+				className="transition-transform"
+				color="secondary"
+				name="Michael Bosciglio"
+				size="sm"
+				src="me.jpg"
+				/>
 		</NextUINavbar>
 	);
 };
